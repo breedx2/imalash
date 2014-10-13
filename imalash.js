214914d9f3c919ad897cdbb7592df321d3c2d2bc
Jason Plumb <jason@noisybox.net>
2014-10-12 23:36:50 -0700

    Write formatted outputs ftw.

--- imalash.js
+++ imalash.js
@@ -1,6 +1,7 @@
 var program = require('commander');
 var lwip = require('lwip');
-var getpixels = require("get-pixels");
+var getpixels = require('get-pixels');
+var fs = require('fs');
 
 program.version('0.0.1')
     .option('-w, --width <width>', 'Width of output image')
@@ -20,15 +21,34 @@ lwip.open(filename, function(err, image){
 		.resize(width, height, "cubic")
 		.writeFile('imalash.temp.jpg', function(err){
 			getpixels('imalash.temp.jpg', 'image/jpeg', dumpPixelArray(image));
-			//todo: delete
+			//fs.unlink('imalash.temp.jpg');
 		});
 });
 
+function hexify(pixels, x, y, z){
+	return "0x" + pixels.get(x, y, z).toString(16).toUpperCase();
+}
+
 function dumpPixelArray(image){
 	return function(err, pixels){
-		console.log("Written: " + pixels.shape.slice());
-		for(var x = 0; x < image.width() ; x++){
-			console.log(pixels.data.get(x));
+		var varname = filename.replace(/\..*/, '');
+		process.stdout.write("uint8_t " + varname + " = {");
+		for(var y = 0; y < image.height() ; y++){
+			if(y > 0){
+				process.stdout.write(', ');
+			}
+			process.stdout.write("\n\t{");
+			for(var x = 0; x < image.width() ; x+= 3){
+				if(x > 0){
+					process.stdout.write(', ');
+				}
+				var r = hexify(pixels, x, y, 0);
+				var g = hexify(pixels, x, y, 1);
+				var b = hexify(pixels, x, y, 2);
+				process.stdout.write("{" + r + "," + g + "," + b + "}");
+			}
+			process.stdout.write("}");
 		}
+		console.log("\n};");
 	}
 }

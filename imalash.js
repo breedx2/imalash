8d4de8521e799d0da8b3ceb717e7fcacd6f94f89
Jason Plumb <jason@noisybox.net>
2014-10-12 22:19:42 -0700

    Write to temp file, start dumping

--- imalash.js
+++ imalash.js
@@ -1,4 +1,6 @@
 var program = require('commander');
+var lwip = require('lwip');
+var getpixels = require("get-pixels");
 
 program.version('0.0.1')
     .option('-w, --width <width>', 'Width of output image')
@@ -11,3 +13,22 @@ if(!filename){
 	process.exit(code = 1);
 }
 
+lwip.open(filename, function(err, image){
+	var width = program.width ? program.width : image.width();
+	var height = program.height ? program.height : image.height();
+	image.batch()
+		.resize(width, height, "cubic")
+		.writeFile('imalash.temp.jpg', function(err){
+			getpixels('imalash.temp.jpg', 'image/jpeg', dumpPixelArray(image));
+			//todo: delete
+		});
+});
+
+function dumpPixelArray(image){
+	return function(err, pixels){
+		console.log("Written: " + pixels.shape.slice());
+		for(var x = 0; x < image.width() ; x++){
+			console.log(pixels.data.get(x));
+		}
+	}
+}
